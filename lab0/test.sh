c2f2df787cd75c90920f893448c1c874bec61d4b
Theo S <theo_s@berkeley.edu>
2022-08-26 17:37:07 -0700

    Upload Lab 0

---
+++ lab0/test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+export base_pth=$1
+
+
+check_file () {
+  if [ -e $1 ]
+  then
+    return 0
+  else
+    return 1
+fi
+}
+
+check_dir () {
+  if [ -d $1 ]
+  then
+    return 0
+  else
+    return 1
+fi
+}
+
+export md_file=$base_pth/lab0.rmd
+export md_file_2=$base_pth/lab0.Rmd
+
+if check_file $md_file
+  then
+    :
+  else
+    if check_file $md_file_2
+    then
+      export md_file=$md_file_2
+    else
+        echo 'lab0 markdown file missing'
+    fi
+fi
+
+export complie_file=$base_pth/rmd_compile.pdf
+Rscript -e "rmarkdown::render('$md_file', output_file ='$complie_file')" --verbose False &> $base_pth/md_log.txt
+if [ $? -eq 0 ]; then
+    rm $base_pth/md_log.txt
+    rm $complie_file
+else
+    echo "Markdown file $md_file does not compile, check $base_pth/md_log.txt"
+fi
+
+
+if check_file $base_pth/lab0.pdf
+  then
+    :
+  else
+    echo "lab 0 pdf file missing"
+
+fi
